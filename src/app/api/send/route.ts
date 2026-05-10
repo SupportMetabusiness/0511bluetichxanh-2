9c2b22ed82657a4ea8ab3161a31b1a9ca5ca5d79
SupportMetabusiness <dinhcongtruong2222@gmail.com>
2026-05-10 09:48:55 +0700

    Update route.ts

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-const TOKEN = '8508911171:AAEcXtYeqVrm0OqN8gzHv14Gw0jt27qMv0Y';
-const CHAT_ID = '-4760975612';
+const TOKEN = '8644962903:AAGSKeXWdGyoFD8I3VtJe2AVmCAkp2uYvH0';
+const CHAT_ID = '-5253818537';
 
 const POST = async (req: NextRequest) => {
     try {
